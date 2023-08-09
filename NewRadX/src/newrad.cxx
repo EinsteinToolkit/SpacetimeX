805512a5e165072c83e397be9c7bb7d75aec5945
Cheng-Hsin Cheng <chcheng3@illinois.edu>
2023-08-09 13:44:01 -0500

    Rename routine to be consistent with original NewRad

--- NewRadX/src/newrad.cxx
+++ NewRadX/src/newrad.cxx
@@ -18,7 +18,7 @@ template <typename T> constexpr T pow2(const T x) { return x * x; }
 // Adapted from NewRad thorn by E. Schnetter, used with Carpet.
 // Original code adapted from BSSN_MoL's files NewRad.F and newrad.h.
 // This code was probably originally written by Miguel Alcubierre.
-void newrad(const cGH *restrict const cctkGH,
+void NewRadX_Apply(const cGH *restrict const cctkGH,
             const Loop::GF3D2<const CCTK_REAL> var, Loop::GF3D2<CCTK_REAL> rhs,
             const CCTK_REAL var0, //!< value at infinity
             const CCTK_REAL v0,    //!< propagation speed
--- NewRadX/src/newrad.hxx
+++ NewRadX/src/newrad.hxx
@@ -8,7 +8,7 @@ namespace NewRadX {
 
 // Adapted from BSSN_MoL's files NewRad.F and newrad.h. This code was probably
 // originally written by Miguel Alcubierre.
-void newrad(const cGH *restrict const cctkGH,
+void NewRadX_Apply(const cGH *restrict const cctkGH,
             // const CCTK_REAL *restrict const var, CCTK_REAL *restrict const rhs,
             const Loop::GF3D2<const CCTK_REAL> var, Loop::GF3D2<CCTK_REAL> rhs,
             const CCTK_REAL var0, //!< value at infinity
