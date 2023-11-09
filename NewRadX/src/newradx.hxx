4d9271451168298e01590cee1a4af2a6c521c30c
Liwei Ji <jiliwei.phys@gmail.com>
2023-11-09 16:16:52 +0000

    NewRadX: Clang-format files

--- NewRadX/src/newradx.hxx
+++ NewRadX/src/newradx.hxx
@@ -9,9 +9,10 @@ namespace NewRadX {
 // Adapted from BSSN_MoL's files NewRad.F and newrad.h. This code was probably
 // originally written by Miguel Alcubierre.
 void NewRadX_Apply(const cGH *restrict const cctkGH,
-            const Loop::GF3D2<const CCTK_REAL> var, Loop::GF3D2<CCTK_REAL> rhs,
-            const CCTK_REAL var0, //!< value at infinity
-            const CCTK_REAL v0,    //!< propagation speed
-      const CCTK_REAL radpower //!< radial fall-off exponent
+                   const Loop::GF3D2<const CCTK_REAL> var,
+                   Loop::GF3D2<CCTK_REAL> rhs,
+                   const CCTK_REAL var0,    //!< value at infinity
+                   const CCTK_REAL v0,      //!< propagation speed
+                   const CCTK_REAL radpower //!< radial fall-off exponent
 );
 } // namespace NewRadX
