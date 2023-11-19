bde6969f1eb7a615ff5e3056731b8d068f10c2fd
Liwei Ji <jiliwei.phys@gmail.com>
2023-11-19 14:21:50 -0600

    Z4c: Add par for falloff order in radiative boundary condition

--- Z4c/src/apply_newrad.cxx
+++ Z4c/src/apply_newrad.cxx
@@ -14,28 +14,28 @@ extern "C" void apply_boundary_conditions(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_apply_boundary_conditions;
   DECLARE_CCTK_PARAMETERS;
 
-  NewRadX_Apply(cctkGH, chi, chi_rhs, 1.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, gammatxx, gammatxx_rhs, 1.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, gammatxy, gammatxy_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, gammatxz, gammatxz_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, gammatyy, gammatyy_rhs, 1.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, gammatyz, gammatyz_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, gammatzz, gammatzz_rhs, 1.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Kh, Kh_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Atxx, Atxx_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Atxy, Atxy_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Atxz, Atxz_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Atyy, Atyy_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Atyz, Atyz_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Atzz, Atzz_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Gamtx, Gamtx_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Gamty, Gamty_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Gamtz, Gamtz_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, Theta, Theta_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, alphaG, alphaG_rhs, 1.0, 1.4142135623730951, 1);
-  NewRadX_Apply(cctkGH, betaGx, betaGx_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, betaGy, betaGy_rhs, 0.0, 1.0, 1);
-  NewRadX_Apply(cctkGH, betaGz, betaGz_rhs, 0.0, 1.0, 1);
+  NewRadX_Apply(cctkGH, chi, chi_rhs, 1.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, gammatxx, gammatxx_rhs, 1.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, gammatxy, gammatxy_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, gammatxz, gammatxz_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, gammatyy, gammatyy_rhs, 1.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, gammatyz, gammatyz_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, gammatzz, gammatzz_rhs, 1.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Kh, Kh_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Atxx, Atxx_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Atxy, Atxy_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Atxz, Atxz_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Atyy, Atyy_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Atyz, Atyz_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Atzz, Atzz_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Gamtx, Gamtx_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Gamty, Gamty_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Gamtz, Gamtz_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, Theta, Theta_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, alphaG, alphaG_rhs, 1.0, 1.4142135623730951, n_falloff);
+  NewRadX_Apply(cctkGH, betaGx, betaGx_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, betaGy, betaGy_rhs, 0.0, 1.0, n_falloff);
+  NewRadX_Apply(cctkGH, betaGz, betaGz_rhs, 0.0, 1.0, n_falloff);
 }
 
 } // namespace Z4c
