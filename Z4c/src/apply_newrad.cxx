3433f13f971029d8b8e1ce138a6c2880b93d77ae
Liwei Ji <jiliwei.phys@gmail.com>
2023-11-21 20:12:06 +0000

    Z4c: Modify field value at infinity for apply_boundary_conditions

--- Z4c/src/apply_newrad.cxx
+++ Z4c/src/apply_newrad.cxx
@@ -14,14 +14,14 @@ extern "C" void apply_boundary_conditions(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_apply_boundary_conditions;
   DECLARE_CCTK_PARAMETERS;
 
-  NewRadX_Apply(cctkGH, chi, chi_rhs, 0, 1, 1);
-  NewRadX_Apply(cctkGH, gammatxx, gammatxx_rhs, 0, 1, 1);
+  NewRadX_Apply(cctkGH, chi, chi_rhs, 1, 1, 1);
+  NewRadX_Apply(cctkGH, gammatxx, gammatxx_rhs, 1, 1, 1);
   NewRadX_Apply(cctkGH, gammatxy, gammatxy_rhs, 0, 1, 1);
   NewRadX_Apply(cctkGH, gammatxz, gammatxz_rhs, 0, 1, 1);
-  NewRadX_Apply(cctkGH, gammatyy, gammatyy_rhs, 0, 1, 1);
+  NewRadX_Apply(cctkGH, gammatyy, gammatyy_rhs, 1, 1, 1);
   NewRadX_Apply(cctkGH, gammatyz, gammatyz_rhs, 0, 1, 1);
-  NewRadX_Apply(cctkGH, gammatzz, gammatzz_rhs, 0, 1, 1);
-  NewRadX_Apply(cctkGH, Kh, Kh_rhs, 0, 1, 1);
+  NewRadX_Apply(cctkGH, gammatzz, gammatzz_rhs, 1, 1, 1);
+  NewRadX_Apply(cctkGH, Kh, Kh_rhs, 0, 1, 2);
   NewRadX_Apply(cctkGH, Atxx, Atxx_rhs, 0, 1, 2);
   NewRadX_Apply(cctkGH, Atxy, Atxy_rhs, 0, 1, 2);
   NewRadX_Apply(cctkGH, Atxz, Atxz_rhs, 0, 1, 2);
