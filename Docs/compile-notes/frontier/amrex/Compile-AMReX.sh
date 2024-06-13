22260f23e5be15cae1259d78ed355089fdc595dc
Liwei Ji <jiliwei.phys@gmail.com>
2024-06-13 11:37:49 -0400

    Docs: add compile notes for frontier

--- Docs/compile-notes/frontier/amrex/Compile-AMReX.sh
+++ Docs/compile-notes/frontier/amrex/Compile-AMReX.sh
@@ -1,6 +1,6 @@
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_INSTALL_PREFIX=${HOME}/local \
-      -DCMAKE_PREFIX_PATH='/opt/rocm-5.3.0/lib/cmake/AMDDeviceLibs;/opt/rocm-5.3.0/lib/cmake/amd_comgr;/opt/rocm-5.3.0/lib/cmake/hip;/opt/rocm-5.3.0/lib/cmake/hiprand;/opt/rocm-5.3.0/lib/cmake/hsa-runtime64;/opt/rocm-5.3.0/lib/cmake/rocprim;/opt/rocm/lib/cmake/rocrand' \
+      -DCMAKE_PREFIX_PATH='/opt/rocm-6.0.0/lib/cmake/AMDDeviceLibs;/opt/rocm-6.0.0/lib/cmake/amd_comgr;/opt/rocm-6.0.0/lib/cmake/hip;/opt/rocm-6.0.0/lib/cmake/hiprand;/opt/rocm-6.0.0/lib/cmake/hsa-runtime64;/opt/rocm-6.0.0/lib/cmake/rocprim;/opt/rocm/lib/cmake/rocrand' \
       -DAMReX_GPU_BACKEND=HIP \
       -DAMReX_AMD_ARCH=gfx90a \
       -DAMReX_FORTRAN=OFF \
---
+++ Docs/compile-notes/frontier/amrex/README.md
@@ -0,0 +1,12 @@
+# Compile on Frontier
+
+## Compile AMReX
+
+```
+cd amrex
+mkdir build
+cd build
+. Load-Module-AMReX.sh
+. Export-AMReX.sh
+. Compile-AMReX.sh
+```
