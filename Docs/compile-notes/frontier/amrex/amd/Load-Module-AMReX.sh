91104fd555e8d45039f78913b70af3aea8a76c54
Liwei Ji <jiliwei.phys@gmail.com>
2024-06-15 15:50:44 -0400

    Docs: update amd/Load-Module-AMReX.sh for frontier

--- Docs/compile-notes/frontier/amrex/amd/Load-Module-AMReX.sh
+++ Docs/compile-notes/frontier/amrex/amd/Load-Module-AMReX.sh
@@ -2,8 +2,9 @@
 
 module purge
 module load PrgEnv-cray/8.5.0
-module load amd-mixed/6.0.0
+module load cce/17.0.0
 module load cpe/23.12
+module load amd-mixed/6.0.0
 module load craype-accel-amd-gfx90a
 module load craype-x86-trento
 module unload darshan-runtime
