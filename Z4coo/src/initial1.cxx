48d3a3c562ef2f058d643b439e6746a8f333d530
Liwei Ji <jiliwei.phys@gmail.com>
2024-07-10 14:40:28 -0400

    Z4coo: update .cxx files taking accout of evolving diff vars

--- Z4coo/src/initial1.cxx
+++ Z4coo/src/initial1.cxx
@@ -74,6 +74,8 @@ extern "C" void Z4coo_Initial1(CCTK_ARGUMENTS) {
   typedef simdl<CCTK_REAL> vbool;
   constexpr size_t vsize = tuple_size_v<vreal>;
 
+  const auto delta3 = one<smat<vreal, 3> >()();
+
   const Loop::GridDescBaseDevice grid(cctkGH);
 #ifdef __CUDACC__
   const nvtxRangeId_t range = nvtxRangeStartA("Z4coo_Initial1::initial1");
@@ -84,7 +86,7 @@ extern "C" void Z4coo_Initial1(CCTK_ARGUMENTS) {
         const GF3D2index index1(layout1, p.I);
 
         // Load
-        const smat<vreal, 3> g = gf_g1(mask, index1, one<smat<int, 3>>()());
+        const smat<vreal, 3> g = gf_g1(mask, index1, one<smat<int, 3> >()());
         const smat<vreal, 3> K = gf_K1(mask, index1);
         const vreal alp = gf_alp1(mask, index1, 1);
         const vec<vreal, 3> beta = gf_beta1(mask, index1);
@@ -93,10 +95,11 @@ extern "C" void Z4coo_Initial1(CCTK_ARGUMENTS) {
         const vreal detg = calc_det(g);
         const smat<vreal, 3> gu = calc_inv(g, detg);
 
-        const vreal chi = 1 / cbrt(detg);
+        const vreal chi = 1 / cbrt(detg) - 1;
 
-        const smat<vreal, 3> gammat([&](int a, int b)
-                                        ARITH_INLINE { return chi * g(a, b); });
+        const smat<vreal, 3> gammat([&](int a, int b) ARITH_INLINE {
+          return (1 + chi) * g(a, b) - delta3(a, b);
+        });
 
         const vreal trK = sum_symm<3>(
             [&](int x, int y) ARITH_INLINE { return gu(x, y) * K(x, y); });
@@ -106,10 +109,10 @@ extern "C" void Z4coo_Initial1(CCTK_ARGUMENTS) {
         const vreal Kh = trK - 2 * Theta;
 
         const smat<vreal, 3> At([&](int a, int b) ARITH_INLINE {
-          return chi * (K(a, b) - trK / 3 * g(a, b));
+          return (1 + chi) * (K(a, b) - trK / 3 * g(a, b));
         });
 
-        const vreal alphaG = alp;
+        const vreal alphaG = alp - 1;
 
         const vec<vreal, 3> betaG([&](int a) ARITH_INLINE { return beta(a); });
 
