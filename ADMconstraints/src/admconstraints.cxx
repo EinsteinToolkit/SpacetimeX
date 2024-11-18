e0141d5263a666ccf4fe95832e78cf7105e63bfc
Liwei Ji <jiliwei.phys@gmail.com>
2024-11-18 16:15:33 -0500

    ADMconstraints: update admconstraints.cxx

--- ADMconstraints/src/admconstraints.cxx
+++ ADMconstraints/src/admconstraints.cxx
@@ -6,6 +6,11 @@ namespace ADMconstraints {
 using namespace Arith;
 using namespace Loop;
 
+template <typename T>
+CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T Power(T x, int y) {
+  return (y == 2) ? Arith::pow2(x) : Arith::pown(x, y);
+}
+
 extern "C" void ADMconstraints(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_ADMconstraints;
   DECLARE_CCTK_PARAMETERS;
