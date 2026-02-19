ebdb52f99e6a38554568cdd3112493c80d4ed0a2
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2026-02-19 09:05:34 -0600

    NewRadX: Removed explicit references to CapyrX

--- NewRadX/src/newradx.cxx
+++ NewRadX/src/newradx.cxx
@@ -4,8 +4,6 @@
 #include <loop_device.hxx>
 #include <driver.hxx>
 
-#include <global_derivatives.hxx>
-
 #include "newradx.hxx"
 
 #include <cmath>
@@ -209,8 +207,6 @@ void NewRadX_Apply(const cGH *restrict const cctkGH,
                    const Loop::GF3D2<const CCTK_REAL> &vJ_dc_dz,
                    const CCTK_REAL var0, const CCTK_REAL v0,
                    const CCTK_REAL radpower) {
-  using namespace CapyrX::MultiPatch::GlobalDerivatives;
-
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
 
